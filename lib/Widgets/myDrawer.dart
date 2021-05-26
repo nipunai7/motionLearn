94b8f2720ea1968f17a95101afbe83fb16256c70
Nipuna Muansinghe <mnipunai7@gmail.com>
2021-05-27 02:34:30 +0530

    1.0

--- lib/Widgets/myDrawer.dart
+++ lib/Widgets/myDrawer.dart
@@ -16,7 +16,86 @@ class MyDrawer extends StatelessWidget {
         children: [
           Container(
             padding: EdgeInsets.only(top: 25.0, bottom: 10.0),
-            color: Colors.deepPurple,
+            decoration: new BoxDecoration(
+              color: Colors.deepPurple,
+            ),
+              child: Column(
+                children: [
+                  Material(
+                    borderRadius: BorderRadius.all(Radius.circular(80.0),),
+                    elevation: 8.0,
+                    child: Container(
+                      height: 160.0,
+                      width: 160.0,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(EcommerceApp.sharedPreferences.getString(EcommerceApp.userAvatarUrl)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0,),
+                  Text(
+                    EcommerceApp.sharedPreferences.getString(EcommerceApp.userName),style: TextStyle(fontFamily: "Signatra",fontSize: 30.0,color: Colors.white),
+                  )
+                ],
+              ),
+          ),
+          SizedBox(height: 6.0,),
+          Container(
+            padding: EdgeInsets.only(top: 1.0),
+            decoration: new BoxDecoration(
+              color: Colors.deepPurple,
+            ),
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.home,color: Colors.white,),
+                  title: Text("Home",style: TextStyle(color: Colors.white),),
+                  onTap: (){
+                    Route route = MaterialPageRoute(builder: (c) => StoreHome());
+                    Navigator.pushReplacement(context, route);
+                  },
+                ),
+                Divider(height: 10.0,color: Colors.white,thickness: 6.0,),
+                ListTile(
+                  leading: Icon(Icons.monetization_on_sharp,color: Colors.white,),
+                  title: Text("My Orders",style: TextStyle(color: Colors.white),),
+                  onTap: (){
+                    Route route = MaterialPageRoute(builder: (c) => MyOrders());
+                    Navigator.pushReplacement(context, route);
+                  },
+                ),
+                Divider(height: 10.0,color: Colors.white,thickness: 6.0,),
+                ListTile(
+                  leading: Icon(Icons.shopping_cart,color: Colors.white,),
+                  title: Text("My Cart",style: TextStyle(color: Colors.white),),
+                  onTap: (){
+                    Route route = MaterialPageRoute(builder: (c) => CartPage());
+                    Navigator.pushReplacement(context, route);
+                  },
+                ),
+                Divider(height: 10.0,color: Colors.white,thickness: 6.0,),
+                ListTile(
+                  leading: Icon(Icons.search,color: Colors.white,),
+                  title: Text("Search",style: TextStyle(color: Colors.white),),
+                  onTap: (){
+                    Route route = MaterialPageRoute(builder: (c) => SearchProduct());
+                    Navigator.pushReplacement(context, route);
+                  },
+                ),
+                Divider(height: 10.0,color: Colors.white,thickness: 6.0,),
+                ListTile(
+                  leading: Icon(Icons.logout,color: Colors.white,),
+                  title: Text("Logout",style: TextStyle(color: Colors.white),),
+                  onTap: (){
+                    EcommerceApp.auth.signOut().then((c){
+                      Route route = MaterialPageRoute(builder: (c) => AuthenticScreen());
+                      Navigator.pushReplacement(context, route);
+                    });
+                  },
+                ),
+                Divider(height: 10.0,color: Colors.white,thickness: 6.0,),
+              ],
+            ),
           )
         ],
       ),
